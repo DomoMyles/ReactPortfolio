5ddc34e3e76bcf515a594247e07bb88d0783816d
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2022-11-25 14:21:37 -0500

    The npm run build works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import Navbar from "./Navbar";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
-import Home  from "./pages/Home";
-import { Route, Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom"
 
 
 function App() {
 
-    return(
+    return (
         <>
-        <Navbar />
-        <div className="container">
-        <Routes>
-        <Route path='/' element={< Home />} />
-        <Route path='/pricing' element={< Projects />} />
-        <Route path='/about' element={< About />} />
-
-            </Routes>
-        </div>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path='/' element={< Home />} />
+                    <Route path='/projects' element={< Projects />} />
+                    <Route path='/about' element={< About />} />
+                </Routes>
+            </div>
         </>
     )
 }
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,5 +1,156 @@
-export default function Home() {
-    return <h1>Projects
-        
+export default function Projects() {
+    return <h1>
+        <div id="Introduction" class="leftPic">
+
+            <h1>Introduction<img src="assets/pic/image.1.png" alt="A picture of me" /></h1>
+            <p>
+                I got into web development in 2018 when I took a web development class. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada
+                pellentesque elit eget gravida cum sociis. Eu lobortis elementum nibh tellus molestie nunc non blandit
+                massa. Nec ullamcorper sit amet risus nullam eget felis eget nunc. Pretium vulputate sapien nec sagittis
+                aliquam malesuada bibendum arcu. Faucibus turpis in eu mi bibendum neque egestas congue quisque. Placerat in
+                egestas erat imperdiet sed. Tristique risus nec feugiat in. Posuere morbi leo urna molestie at elementum eu
+                facilisis sed. Donec massa sapien faucibus et molestie ac feugiat sed. At in tellus integer feugiat
+                scelerisque varius. Morbi enim nunc faucibus a. Interdum velit laoreet id donec ultrices tincidunt arcu.
+                Felis donec et odio pellentesque diam. Purus sit amet volutpat consequat mauris nunc congue nisi. Tristique
+                senectus et netus et malesuada fames ac turpis egestas. Integer feugiat scelerisque varius morbi. Imperdiet
+                nulla malesuada pellentesque elit eget gravida cum sociis natoque.
+
+                Sapien faucibus et molestie ac feugiat sed. Morbi tincidunt ornare massa eget egestas purus viverra. Varius
+                duis at consectetur lorem donec massa sapien faucibus et. Viverra vitae congue eu consequat ac. Aliquam sem
+                fringilla ut morbi tincidunt augue interdum velit euismod. Tincidunt dui ut ornare lectus sit. Adipiscing
+                enim eu turpis egestas. Elit eget gravida cum sociis natoque penatibus. Purus faucibus ornare suspendisse
+                sed nisi lacus sed viverra tellus. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.
+                Sodales neque sodales ut etiam sit amet. Mattis aliquam faucibus purus in massa tempor nec. Leo integer
+                malesuada nunc vel risus commodo viverra maecenas. Quam viverra orci sagittis eu volutpat odio facilisis
+                mauris sit. Integer quis auctor elit sed vulputate mi sit amet mauris. Sit amet volutpat consequat mauris
+                nunc congue nisi vitae. Eu ultrices vitae auctor eu. Ornare suspendisse sed nisi lacus. Mauris commodo quis
+                imperdiet massa tincidunt nunc pulvinar. Arcu vitae elementum curabitur vitae nunc sed velit dignissim
+                sodales.
+            </p>
+        </div>
+
+
+
+        <div id="Projects" class="rightPic">
+            <h1>Html Projects
+                <a href="Milo_s website - Copy/HTML website.html" target="_blank">
+                    <img src="assets/pic/Image5.JPG" alt="DomoFace" />
+                </a>
+            </h1>
+            <p>
+                This website is a website I made back in highschool while i was in a coding class. I built this in
+                Notepad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis. Eu lobortis elementum nibh
+                tellus molestie nunc non blandit massa. Nec ullamcorper sit amet risus nullam eget felis eget nunc. Pretium
+                vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Faucibus turpis in eu mi bibendum neque
+                egestas congue quisque. Placerat in egestas erat imperdiet sed. Tristique risus nec feugiat in. Posuere
+                morbi leo urna molestie at elementum eu facilisis sed. Donec massa sapien faucibus et molestie ac feugiat
+                sed. At in tellus integer feugiat scelerisque varius. Morbi enim nunc faucibus a. Interdum velit laoreet id
+                donec ultrices tincidunt arcu. Felis donec et odio pellentesque diam. Purus sit amet volutpat consequat
+                mauris nunc congue nisi. Tristique senectus et netus et malesuada fames ac turpis egestas. Integer feugiat
+                scelerisque varius morbi. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque.
+
+                Sapien faucibus et molestie ac feugiat sed. Morbi tincidunt ornare massa eget egestas purus viverra. Varius
+                duis at consectetur lorem donec massa sapien faucibus et. Viverra vitae congue eu consequat ac. Aliquam sem
+                fringilla ut morbi tincidunt augue interdum velit euismod. Tincidunt dui ut ornare lectus sit. Adipiscing
+                enim eu turpis egestas. Elit eget gravida cum sociis natoque penatibus. Purus faucibus ornare suspendisse
+                sed nisi lacus sed viverra tellus. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.
+                Sodales neque sodales ut etiam sit amet. Mattis aliquam faucibus purus in massa tempor nec. Leo integer
+                malesuada nunc vel risus commodo viverra maecenas. Quam viverra orci sagittis eu volutpat odio facilisis
+                mauris sit. Integer quis auctor elit sed vulputate mi sit amet mauris. Sit amet volutpat consequat mauris
+                nunc congue nisi vitae. Eu ultrices vitae auctor eu. Ornare suspendisse sed nisi lacus. Mauris commodo quis
+                imperdiet massa tincidunt nunc pulvinar. Arcu vitae elementum curabitur vitae nunc sed velit dignissim
+                sodales.
+            </p>
+        </div>
+
+
+
+        <div id="Games" class="leftPic">
+            <h1>Video Games
+
+                <img src="assets/pic/image6.JPG" alt="DomoFace" />
+
+
+            </h1>
+            <p>
+                This is a game I made for a class in 2017 for a project in school. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada pellentesque
+                elit eget gravida cum sociis. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Nec
+                ullamcorper sit amet risus nullam eget felis eget nunc. Pretium vulputate sapien nec sagittis aliquam
+                malesuada bibendum arcu. Faucibus turpis in eu mi bibendum neque egestas congue quisque. Placerat in egestas
+                erat imperdiet sed. Tristique risus nec feugiat in. Posuere morbi leo urna molestie at elementum eu
+                facilisis sed. Donec massa sapien faucibus et molestie ac feugiat sed. At in tellus integer feugiat
+                scelerisque varius. Morbi enim nunc faucibus a. Interdum velit laoreet id donec ultrices tincidunt arcu.
+                Felis donec et odio pellentesque diam. Purus sit amet volutpat consequat mauris nunc congue nisi. Tristique
+                senectus et netus et malesuada fames ac turpis egestas. Integer feugiat scelerisque varius morbi. Imperdiet
+                nulla malesuada pellentesque elit eget gravida cum sociis natoque.
+
+                Sapien faucibus et molestie ac feugiat sed. Morbi tincidunt ornare massa eget egestas purus viverra. Varius
+                duis at consectetur lorem donec massa sapien faucibus et. Viverra vitae congue eu consequat ac. Aliquam sem
+                fringilla ut morbi tincidunt augue interdum velit euismod. Tincidunt dui ut ornare lectus sit. Adipiscing
+                enim eu turpis egestas. Elit eget gravida cum sociis natoque penatibus. Purus faucibus ornare suspendisse
+                sed nisi lacus sed viverra tellus. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices.
+                Sodales neque sodales ut etiam sit amet. Mattis aliquam faucibus purus in massa tempor nec. Leo integer
+                malesuada nunc vel risus commodo viverra maecenas. Quam viverra orci sagittis eu volutpat odio facilisis
+                mauris sit. Integer quis auctor elit sed vulputate mi sit amet mauris. Sit amet volutpat consequat mauris
+                nunc congue nisi vitae. Eu ultrices vitae auctor eu. Ornare suspendisse sed nisi lacus. Mauris commodo quis
+                imperdiet massa tincidunt nunc pulvinar. Arcu vitae elementum curabitur vitae nunc sed velit dignissim
+                sodales.
+            </p>
+        </div>
+
+
+
+        <div id="WES" class="rightPic">
+            <h1>Proximity
+                <a href="Milo_s website - Copy/HTML website.html" target="_blank">
+                    <img src="assets/pic/GroupProjectPicture.JPG" alt="MapWebsite similar to google maps" />
+                </a>
+            </h1>
+            <p>
+                Description
+                a location based navigation application designed to give users an alternative way to use maps. The main
+                purpose of Proximity is to view the available options nearby a chosen destination. On vacation and need to
+                find an ATM nearby your hotel? Traveling for work and need close access to a pharmacy? Our app will help you
+                view and locate the options available to you.
+
+                The app can be used by entering in a location in the U.S., either a general state or city, or even a
+                specific address. After choosing a category (i.e. Restaurant, Bank, or Pharmacy) and clicking Search, a map
+                on the right side of the screen will populate with the location and all of the nearby locations matching the
+                category, within a specific distance radius in relation to the user input.
+
+                The primary areas of code and technology that were focused are as follows:
+
+                We used the Bulma framework to aid in our CSS organization and creation
+                Through the Google Maps Platform, we utilized various APIs from Maps, Routes, and Places
+                Navigated and edited extensive JavaScript code in order to utilized Google's Maps Platform API
+                Used various pseudo variables to create custom color variables in the CSS, colors that follow a strict color
+                palette that matches our project presentation
+            </p>
+        </div>
+
+
+
+        <div id="WES" class="leftPic">
+            <h1>TAC
+                <a href="https://enigmatic-fortress-43863.herokuapp.com/home" target="_blank">
+                    <img src="assets\pic\picture6.JPG" alt="picture of desk with stuff on it" />
+                </a>
+
+            </h1>
+            <p>
+                Description
+                The Advertising Campaign (TAC), is a marketing webtool for administrators to promote their startup business.
+                This application provides a secure login, minimalistic user interface, and a commercial email marketing
+                service to send personalized emails to our customers.
+
+                Motovation
+                Our motovation was to help the potential clients to promote their business by sending beautifully designed emails to the
+                subscribed customers. Importantly, users like our application.
+            </p>
+        </div>
     </h1>
+
 }
