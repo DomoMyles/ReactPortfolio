8d88f353eb9504c69f5d64f5b59a4865853e1a6f
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2023-07-13 18:52:04 -0400

    Fix Spelling

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,11 +5,7 @@ import image2 from '../pic/GamePic.JPG'
 import image3 from '../pic/TacPic.JPG'
 import image4 from '../pic/GroupProjectPicture.JPG'
 import image5 from '../pic/EmployePic.png'
-<<<<<<< HEAD
 import image8 from '../pic/EmployePic.png'
-=======
-import image8 from '../pic/CookiePic.png'
->>>>>>> 5e9e085208a089685da17b9b24210775c86bc65f
 
 
 export default function Projects() {
@@ -121,9 +117,6 @@ export default function Projects() {
             </p>
         </div>
         <br></br>
-<<<<<<< HEAD
     </div>
-=======
->>>>>>> 5e9e085208a089685da17b9b24210775c86bc65f
     </div>
 }
